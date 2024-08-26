31cf48f58e23899389ca839ef04adfa5009e0fc3
wido2 <wid.o2@live.com>
2024-08-26 12:44:09 +0700

    new update

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -17,6 +20,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
 
+     public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@pt-kbm.com');
+    }
 
 
 
